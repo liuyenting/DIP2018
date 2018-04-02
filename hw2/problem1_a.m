78d70a74c667c4589c6bf4313583f13ec20b6770
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-04-02 19:11:24 +0800

    Add Sobel operator

--- hw2/problem1_a.m
+++ hw2/problem1_a.m
@@ -68,6 +68,31 @@ title('I_{1xy}');
 set(gca, 'FontSize', 14);
 
 %% Sobel
+figure('Name', 'Sobel Filter', 'NumberTitle', 'off');
+subplot(1, 4, 1);
+imshow(I1);
+title('I_1');
+set(gca, 'FontSize', 14);
+
+I1x = imconv2(I1, [1, 0, -1; 2, 0, -2; 1, 0, -1]);
+I1y = imconv2(I1, [1, 2, 1; 0, 0, 0; -1, -2, -1]);
+
+subplot(1, 4, 2);
+imshow(I1x, []);
+title('I_{1x}');
+set(gca, 'FontSize', 14);
+
+subplot(1, 4, 3);
+imshow(I1y, []);
+title('I_{1y}');
+set(gca, 'FontSize', 14);
+
+I1xy = sqrt(I1x.^2 + I1y.^2);
+
+subplot(1, 4, 4);
+imshow(I1xy, []);
+title('I_{1xy}');
+set(gca, 'FontSize', 14);
 
 %% 2nd order edge detection
 
